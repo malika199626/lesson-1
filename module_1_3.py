253e250a63e468a9e2826c43c9423f11b66f0a26
malika199626 <vfkbrf1996@yandex.ru>
2025-01-16 16:23:43 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Малика"
+print(name)
+age = 28
+print(age)
+age=age+3
+print(age)
+is_student = True
+print(is_student)
---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+count_of_homework = 12
+spent_time = 1.5
+name_of_course = 'Python'
+time_to_homework =  spent_time  / count_of_homework
+
+print('Курс:', name_of_course, 'всего задач',count_of_homework, 'затрачено часов:', spent_time, 'среднее время выполнения', time_to_homework,'часов')
