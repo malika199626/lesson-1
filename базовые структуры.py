22ae40fcdbc94a89f8744f7ed09351f41f90d42e
malika199626 <vfkbrf1996@yandex.ru>
2025-01-16 15:48:45 +0300

    Add files via upload

---
+++ базовые структуры.py
@@ -0,0 +1,14 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#4th program
+x=str(123.456)
+x2=123.456*10
+x2=int(x2)%10
+print(x2)
+
